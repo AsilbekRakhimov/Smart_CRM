2d031913e764f35efca8cf051dd21a72f8430704
AsilbekRakhimov <asilbekrakhimov5@gmail.com>
2024-10-08 20:45:54 +0500

    auth part is updated

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -4,12 +4,7 @@ class AuthService {
   constructor() {}
 
   async logInFn({ username, password }) {
-    // console.log(password, username);
-    // console.log(process.env.USERNAME, process.env.PASSWORD);
-    if (
-      (username == process.env.ADMIN || "admin") &&
-      (password == process.env.PASSWORD || "12345")
-    ) {
+    if (username == "admin" && password == "12345") {
       const accessToken = signAccessToken({ id: Date.now() });
       return { accessToken };
     }
