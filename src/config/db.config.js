f522ded769a5544472bbe3e8aa2b92787886d62d
AsilbekRakhimov <asilbekrakhimov5@gmail.com>
2024-10-08 20:26:45 +0500

    db config file is updated

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const dbConfig = {
-  dbUrl: process.env.DATABASE_URL,
+  dbUrl: process.env.DATABASE_URL || "mongodb+srv://asilbek:asil4742@cluster0.7cn8s.mongodb.net/",
 };
