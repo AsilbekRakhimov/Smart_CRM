8c9054bbd3dcf2f41112f309bf50937aa426f21c
AsilbekRakhimov <asilbekrakhimov5@gmail.com>
2024-10-08 20:44:07 +0500

    auth service part is updated

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -7,9 +7,8 @@ class AuthService {
     // console.log(password, username);
     // console.log(process.env.USERNAME, process.env.PASSWORD);
     if (
-      username == process.env.ADMIN ||
-      ("admin" && password == process.env.PASSWORD) ||
-      "12345"
+      (username == process.env.ADMIN || "admin") &&
+      (password == process.env.PASSWORD || "12345")
     ) {
       const accessToken = signAccessToken({ id: Date.now() });
       return { accessToken };
